2542bd5864eb017f412a07e9fa6800c6e098859b
Tanya Shukla <stanya97@gmail.com>
2021-10-05 01:04:00 +0530

    Updating the Healthbars

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,33 @@
+function getRandomValue(min,max) {
+    return Math.floor(Math.random()*(max - min)) + min;
+}
+
+const app = Vue.createApp({
+data(){
+    return{
+playerHealth:100,
+monsterHealth:100
+    };
+},
+computed:{
+    monsterBarStyles(){
+        return {width: this.monsterHealth + '%'};
+    },
+    playerBarStyles(){
+        return {width: this.playerHealth + '%'};
+    }
+},
+methods:{
+    attackMonster(){
+       const attackValue = getRandomValue(5,12);
+       this.monsterHealth -= attackValue;
+       this.attackPlayer();
+  },
+    attackPlayer(){
+        const attackValue = Math.floor(Math.random()*(15-8)) +8;
+        this.playerHealth -= attackValue;
+  }
+}
+});
+
+app.mount('#game');
